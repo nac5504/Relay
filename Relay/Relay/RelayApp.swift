8c0fa300a603fffcebc8235ea3893ec0798e8ae2
nac5504 <ncandello3@gatech.edu>
2026-04-01 20:03:58 -0400

    created blank xcode proj

---
+++ Relay/Relay/RelayApp.swift
@@ -0,0 +1,32 @@
+//
+//  RelayApp.swift
+//  Relay
+//
+//  Created by Nicholas Candello on 4/1/26.
+//
+
+import SwiftUI
+import SwiftData
+
+@main
+struct RelayApp: App {
+    var sharedModelContainer: ModelContainer = {
+        let schema = Schema([
+            Item.self,
+        ])
+        let modelConfiguration = ModelConfiguration(schema: schema, isStoredInMemoryOnly: false)
+
+        do {
+            return try ModelContainer(for: schema, configurations: [modelConfiguration])
+        } catch {
+            fatalError("Could not create ModelContainer: \(error)")
+        }
+    }()
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+        .modelContainer(sharedModelContainer)
+    }
+}
